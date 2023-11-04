99fb1951cc4d4aa96a5d9b10d863c5636a980590
Gastón Camú <camugaston2002@gmail.com>
2023-11-04 19:16:58 -0300

    Creacion de la carpeta clases y K8,M8,UAV

--- clases/EnumEstado.cs
+++ clases/EnumEstado.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TP_Integrador
+namespace TP_Integrador.clases
 {
     internal enum Estado
     {
---
+++ clases/M8.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Integrador.clases
+{
+    internal class M8 : Operador
+    {
+        string id;
+        Bateria bateria;
+        string estado;
+        int cargaActual;
+        int velocidadOptima;
+        Localizacion localizacionBase;
+        Localizacion localizacionActual;
+
+         void Mover()
+        {
+
+        }
+        void VerificarProximidadConOperador()
+        {
+
+        }
+        void VerificarEstado()
+        {
+
+        }
+        void ComprobarBateriaActual()
+        {
+
+        }
+        void DisminuirBateriaActual()
+        {
+
+        }
+        void TransferirCargaBateria()
+        {
+
+        }
+        void TransferirCargaFisica()
+        {
+
+        }
+        void VolverAlCuartel()
+        {
+
+        }
+        void CargarBateria()
+        {
+
+        }
+    }
+}
