67b7c6f148ac2750c05027c8940697d82c1ad82b
AliciaNazar <alicianazarab@gmail.com>
2023-11-04 19:15:09 -0300

    Agregados métodos vacíos a la clase abstracta Operador

--- Operador.cs
+++ Operador.cs
@@ -6,17 +6,52 @@ using System.Threading.Tasks;
 
 namespace TP_Integrador
 {
-    public abstract class Operador
+    internal abstract class Operador
     {
         string id;
         Bateria bateria;
         string estado;
-        double cargaActual;
-        double velocidadOptima;
+        int cargaActual;
+        int velocidadOptima;
         internal Localizacion localizacionBase;
         Localizacion localizacion;
 
+        void Mover()
+        {
 
+        }
+        void VerificarProximidadConOperador() { 
+            
+        }
+
+        void VerificarEstado() { 
+            
+        }
+
+        void ComprobarBateriaActual()
+        {
+
+        }
+        void DisminuirBateriaActual()
+        {
+
+        }
+        void TransferirCargaBateria()
+        {
+
+        }
+        void TransferirCargaFisica()
+        {
+
+        }
+        void VolverAlCuartel()
+        {
+
+        } 
+        void CargarBateria()
+        {
+
+        }
     }
 
 }
