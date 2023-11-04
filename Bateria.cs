d8d27111cc8da107d9aac136e854ce1a6310822b
Gastón Camú <camugaston2002@gmail.com>
2023-11-04 18:51:26 -0300

    Bateria Y Enum de Estado creados

---
+++ Bateria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Integrador
+{
+    internal class Bateria
+    {
+        string id;
+        int cargaMaxima;
+
+
+        void recargarBateria()
+        {
+        }
+    }
+}
