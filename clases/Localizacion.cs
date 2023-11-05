96efc706ee091e74a99d8a4dd58660b36f7d50b3
Gustavo Cantarella <Gus.Cantarella@gmail.com>
2023-11-05 04:56:22 -0300

    Creacion de matriz para localizacion.

--- clases/Localizacion.cs
+++ clases/Localizacion.cs
@@ -15,6 +15,7 @@ namespace TP_Integrador
         public int coordenadaY { get; }
         public TipoLocalizacion tipoLocalizacion { get; }
 
+        
         public Localizacion(string nombre, int coordX, int coordY, TipoLocalizacion tipo)
         {
             nombreLocalizacion = nombre;
@@ -22,5 +23,25 @@ namespace TP_Integrador
             coordenadaY = coordY;
             tipoLocalizacion = tipo;
         }
+
+        // Inicio de la creacion de la matriz para el tablero "imaginario" de coordenadas.
+
+        string[,] mapa = new string[100, 100]; // a modo de ejemplo, la matriz generada aca tiene espacio de 100x100 (estilo hoja cuadriculada, ejemplo: [0,1] = 1km recorrido)
+
+        public void InicializarTableroVacio() // Este metodo debe generar el terreno con todos los espacios vacios, a determinar la ubicacion de cada objeto.
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    mapa[i, j] = "Vacio";
+                }
+            }
+        }
+
+
+        // Fin de la matriz y sus metodos
+
+
     }
 }
