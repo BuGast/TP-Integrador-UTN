c99ba513ed3f566d81424fe15334aff96347cba0
Gastón Camú <camugaston2002@gmail.com>
2023-11-09 07:47:38 -0300

    Se modifico la clase bateria

--- clases/Bateria.cs
+++ clases/Bateria.cs
@@ -6,15 +6,57 @@ using System.Threading.Tasks;
 
 namespace TP_Integrador
 {
-    internal class Bateria
+    public class Bateria
     {
-        string id;
-        int cargaMaxima;
-        int cargaActual;
 
+        protected int cargaMaxima;
+        protected int cargaActual;
 
-        void recargarBateria()
+        public void CargarCargaMaxima(int cargaM)
         {
+            this.cargaMaxima = cargaM;
+            this.cargaActual = cargaMaxima;
         }
+        public void RecargarBateria(int carga)
+        {
+            if (carga > 0)
+            {
+                if ((carga + this.cargaActual) <= this.cargaMaxima)
+                {
+                    this.cargaActual += carga;
+                    Console.WriteLine("Se recargo la bateria");
+                }
+                else
+                {
+                    Console.WriteLine("La energia excede la bateria maxima");
+                }
+            }
+        }
+        public void DescargarBateria(int carga)
+        {
+            if (carga > 0)
+            {
+                if (carga <= this.cargaActual)
+                {
+                    this.cargaActual += carga;
+                    Console.WriteLine("Se descargo la bateria");
+                }
+                else
+                {
+                    Console.WriteLine("Error la descarga es mayor a la carga actual de la bateria");
+                }
+            }
+        }
+
+        public int MostrarCargaMaxima()
+        {
+            return cargaMaxima;
+        }
+        public int MostrarCargaActual()
+        {
+            return cargaActual;
+        }
+
+
     }
 }
