86b4ad2e09cb9a4f57212aafa8a604a3c3aafcf2
Gustavo Cantarella <Gus.Cantarella@gmail.com>
2023-11-23 01:17:22 -0300

    Comentario

--- clases/Cuartel.cs
+++ clases/Cuartel.cs
@@ -1,3 +1,5 @@
+// GUSTAVO MARTIN CANTARELLA
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- clases/GeneradorDeMapa.cs
+++ clases/GeneradorDeMapa.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// GUSTAVO MARTIN CANTARELLA
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
